45c60f826aecf9820ef1e60f4e29b61a86117cb8
lucass2k <lucasnahuel2000silva@gmail.com>
2023-07-04 12:57:37 -0300

    styles/toFixed

--- main.js
+++ main.js
@@ -16,25 +16,25 @@ function myResult() {
   option_to = resultType.value;
 
   if (option_from === "Gota" && option_to === "Microgota") {
-    result.value = Number(input.value) * 3;
+    result.value = Number(input.value * 3).toFixed(2);
   } else if (option_from === "Gota" && option_to === "Cc/Ml") {
-    result.value = Number(input.value) / 20;
+    result.value = Number(input.value / 20).toFixed(2);
   } else if (option_from === "Gota" && option_to === "Gota") {
     result.value = input.value;
   }
 
   if (option_from === "Microgota" && option_to === "Gota") {
-    result.value = Number(input.value) / 3;
+    result.value = Number(input.value / 3).toFixed(2);
   } else if (option_from === "Microgota" && option_to === "Cc/Ml") {
-    result.value = Number(input.value) / 60;
+    result.value = Number(input.value / 60).toFixed(2);
   } else if (option_from === "Microgota" && option_to === "Microgota") {
     result.value = input.value;
   }
 
   if (option_from === "Cc/Ml" && option_to === "Microgota") {
-    result.value = Number(input.value) * 60;
+    result.value = Number(input.value * 60).toFixed(2);
   } else if (option_from === "Cc/Ml" && option_to === "Gota") {
-    result.value = Number(input.value) * 20;
+    result.value = Number(input.value * 20).toFixed(2);
   } else if (option_from === "Cc/Ml" && option_to === "Cc/Ml") {
     result.value = input.value;
   }
